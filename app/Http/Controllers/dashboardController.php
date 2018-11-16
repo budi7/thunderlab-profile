8d559be33ffd9ec64bad630b81e5fa5e17815fbf
budi963 <dev.budi.purnomo@outlook.com>
2018-11-16 10:38:32 +0700

    implement google analytics reporting API

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Guestbook;
 
 use Analytics, Period;
+use Carbon\Carbon;
 
 class dashboardController extends Controller
 {
@@ -16,10 +17,10 @@ class dashboardController extends Controller
 		$this->page_attributes->title       = 'Dashboard';
 		$this->page_attributes->sub_title   = '';
         $this->page_attributes->filter      =  null;
-        
-        // Get analytics data
-        $anal = Analytics::fetchVisitorsAndPageViews(Period::days(7));
 
+        // Get guestbook data
+        $this->page_datas->analytics        = $this->getAnalytics();  
+    
         // Get guestbook data
         $this->page_datas->guestbooks       = Guestbook::where('isContacted', null)
                                                 ->orderBy('created_at', 'desc')
@@ -28,6 +29,33 @@ class dashboardController extends Controller
         // views
         $this->view                         = view('pages.backend.dashboard');
         return $this->generateView(); 
+    }
+
+    private function getAnalytics(){
+        // Get analytics data
+        $analytics = Analytics::fetchVisitorsAndPageViews(Period::days(7));
+
+        if(!$analytics){
+            return response()->json([
+                'code' => '500',
+                'data' => null
+            ]);
+        }
+        
+        // format data
+        $formatted_data = [];
+        foreach ($analytics as $key => $analytic) {
+            $tmp_date = Carbon::createFromFormat('Y-m-d H:i:s', $analytic['date'], 'GMT+7');
+
+            $tmp = [
+                'date' => $tmp_date->format("d M Y"),
+                'visitors' => $analytic['visitors'],
+                'pageViews' => $analytic['pageViews'],
+            ];
+            array_push($formatted_data, $tmp);
+        }
+
+        return $formatted_data;
     }
 
     
--- resources/views/pages/backend/dashboard.blade.php
+++ resources/views/pages/backend/dashboard.blade.php
@@ -16,13 +16,13 @@ active
                         $dt=date_create($datestring);
                         @endphp
                         <h5 class="card-category">{{ $dt->format('d M Y') }} - {{ date("d M Y") }}</h5>
-                        <h2 class="card-title">Monthly Visitor</h2>
+                        <h2 class="card-title">Weekly Visitor</h2>
                     </div>
                 </div>
             </div>
             <div class="card-body">
                 <div class="chart-area">
-                    <canvas id="chartBig1"></canvas>
+                    <canvas id="chartAnalytics"></canvas>
                 </div>
             </div>
         </div>
@@ -78,5 +78,97 @@ active
 
 
 @push('scripts')
-@include('components.backend.clickableRow')
+    @include('components.backend.clickableRow')
+
+    // init chart
+    gradientChartOptionsConfiguration =  {
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        
+        tooltips: {
+            backgroundColor: '#fff',
+            titleFontColor: '#333',
+            bodyFontColor: '#666',
+            bodySpacing: 4,
+            xPadding: 12,
+            mode: "nearest",
+            intersect: 0,
+            position: "nearest"
+        },
+        responsive: true,
+        scales:{
+            yAxes: [{
+                barPercentage: 1.6,
+                gridLines: {
+                    drawBorder: false,
+                    color: 'rgba(29,140,248,0.0)',
+                    zeroLineColor: "transparent",
+                },
+                ticks: {
+                    suggestedMin:50,
+                    suggestedMax: 110,
+                    padding: 20,
+                    fontColor: "#9a9a9a"
+                }
+            }],
+            
+            xAxes: [{
+                barPercentage: 1.6,
+                gridLines: {
+                    drawBorder: false,
+                    color: 'rgba(220,53,69,0.1)',
+                    zeroLineColor: "transparent",
+                },
+                ticks: {
+                    padding: 20,
+                    fontColor: "#9a9a9a"
+                }
+            }]
+        }
+    };
+
+    var ctx = document.getElementById("chartAnalytics").getContext("2d");
+
+    var gradientStroke = ctx.createLinearGradient(0,230,0,50);
+
+    gradientStroke.addColorStop(1, 'rgba(72,72,176,0.2)');
+    gradientStroke.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+    gradientStroke.addColorStop(0, 'rgba(119,52,169,0)'); //purple colors
+
+    var data_source = {!! json_encode($page_datas->analytics) !!};
+    var ds_date = [], ds_visitors = [];
+
+    data_source.forEach(function(element) {
+        ds_date.push(element.date);
+        ds_visitors.push(element.visitors);
+    });
+
+    var data = {
+        labels: ds_date,
+        datasets: [{
+            label: "Visitors",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: '#d048b6',
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: '#d048b6',
+            pointBorderColor:'rgba(255,255,255,0)',
+            pointHoverBackgroundColor: '#d048b6',
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: ds_visitors,
+        }]
+    };
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: gradientChartOptionsConfiguration
+    });    
 @endpush
